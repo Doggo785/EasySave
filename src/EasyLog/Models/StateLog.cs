ead6edca61c56bcf92eef090f5c7740ef2f20693
Doggo785 <stephane.plathey@proton.me>
2026-02-03 09:26:38 +0100

    feat(models): add StateLog model for real-time job tracking
    Introduce the StateLog class to track the current status of backup jobs, including progress percentages, file counts, remaining sizes, and active file paths.

---
+++ src/EasyLog/Models/StateLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyLog.Models
+{
+    public class StateLog
+    {
+        public string JobName { get; set; }
+        public DateTime LastActionTimestamp { get; set; }
+        public string State {  get; set; }
+        public int TotalFilesToCopy { get; set; }
+        public long TotalFilesSize { get; set; }
+        public int NbFilesLeftToDo { get; set; }
+        public long RemainingFilesSize { get; set; }
+        public int Progression { get; set; }
+        public string CurrentSourceFilePath { get; set; }
+        public string CurrentDestinationFilePath { get; set; }
+    }
+}
